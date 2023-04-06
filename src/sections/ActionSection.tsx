a7bb9378003f21bbe9aed03068329c272c16e0fa
Sangwan Jeon <maxswjeon@planetariumhq.com>
2023-04-06 11:06:38 +0900

    feat: list action types

--- src/sections/ActionSection.tsx
+++ src/sections/ActionSection.tsx
@@ -7,31 +7,66 @@ import {
   Heading,
   Input,
   Spacer,
+  Text,
 } from '@chakra-ui/react';
 import { useLib9c } from '../hooks/useLib9c';
+import { Select } from 'chakra-react-select';
+import { useMemo } from 'react';
 
 export function ActionSection() {
   const lib9c = useLib9c();
+
+  const actionTypes = useMemo(() => {
+    if (!lib9c) {
+      return [];
+    }
+
+    return Object.keys(lib9c)
+      .filter((f) => !f.startsWith('deserialize'))
+      .filter((f) => f[0] === f[0].toLowerCase())
+      .filter((f) => !f.startsWith('_'))
+      .filter(
+        (f) =>
+          ![
+            'boot',
+            'default',
+            'attachSignature',
+            'buildUnsignedTransaction',
+            'serializeObjectAsDotnet',
+            'toHex',
+            'parseHex',
+          ].includes(f)
+      )
+      .map((f) => ({ label: f, value: f }));
+  }, [lib9c]);
+
+  if (!lib9c) {
+    return (
+      <Box>
+        <Heading as="h2" size="lg">
+          Create Action
+        </Heading>
+        <Text my="6" textAlign="center">
+          Loading lib9c-wasm...
+        </Text>
+      </Box>
+    );
+  }
 
   return (
     <Box>
       <Heading as="h2" size="lg">
         Create Action
       </Heading>
-      <Flex as="form" gap="3" flexDir="column">
-        <FormControl isRequired>
-          <FormLabel>Reciever Address</FormLabel>
-          <Input placeholder="Reciever Address" />
+      <Box as="form">
+        <FormControl mt="3" isRequired>
+          <FormLabel>Action Type</FormLabel>
+          <Select options={actionTypes} />
         </FormControl>
-        <FormControl isRequired>
-          <FormLabel>Amount</FormLabel>
-          <Input placeholder="Amount" />
-        </FormControl>
-        <Spacer />
-        <Button type="submit" w="full">
+        <Button mt="3" type="submit" w="full">
           Send
         </Button>
-      </Flex>
+      </Box>
     </Box>
   );
 }
